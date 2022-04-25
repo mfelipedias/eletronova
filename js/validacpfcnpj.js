07a2d5f71df1d7a4dcde77b07ee570c92d70b005
mfelipedias <43459240+mfelipedias@users.noreply.github.com>
2022-04-25 09:47:06 -0300

    Valida CPF e CNPJ
    Função unica que verifica o CPF o CNPJ e altera o classe do elemento. (feito em arquivo de teste)

--- js/validacpfcnpj.js
+++ js/validacpfcnpj.js
@@ -1,85 +1,109 @@
-function myFunction() {
-    document.getElementById("retorno").innerHTML = "I have changed!";
-  }
-function validarCPF(cpf) {
-	var Soma;
-	var Resto;
-	Soma = 0;
+function verifica(cpf) {
+    var retorno = document.getElementById("retorno");
+    var cpfcnpj = document.getElementById("cpfcnpj");
+    cpf = cpf.replace(/[^\d]+/g, '');
+    if (cpf == '') return false;
 
-    cpf = cpf.replace(/[^\d]+/g,'');
- 
-    if(cpf == '') return false;
-     
+    if (cpf.length <= 11) {
+        x = validarCPF(cpf);
+        if (x == false) {
+            retorno.innerHTML = "CPF Errado";
+            cpfcnpj.classList.add("bg_1");
+        } else {
+            retorno.innerHTML = "CPF Válido";
+            cpfcnpj.classList.remove("bg_1");
+        }
+    } else {
+        y = validarCNPJ(cpf);
+        if (y == false) {
+            retorno.innerHTML = "CNPJ Errado";
+        } else {
+            retorno.innerHTML = "CNPJ Válido";
+        }
+    }
+
+}
+
+function validarCPF(cpf) {
+
+    var Soma;
+    var Resto;
+    Soma = 0;
+
+    cpf = cpf.replace(/[^\d]+/g, '');
+
+    if (cpf == '') return false;
+
     if (cpf.length != 11)
         return false;
 
-	if (cpf == "00000000000") return false;
+    if (cpf == "00000000000") return false;
 
-	for (i = 1; i <= 9; i++) Soma = Soma + parseInt(cpf.substring(i - 1, i)) * (11 - i);
-	Resto = (Soma * 10) % 11;
+    for (i = 1; i <= 9; i++) Soma = Soma + parseInt(cpf.substring(i - 1, i)) * (11 - i);
+    Resto = (Soma * 10) % 11;
 
-	if ((Resto == 10) || (Resto == 11)) Resto = 0;
-	if (Resto != parseInt(cpf.substring(9, 10))) return false;
+    if ((Resto == 10) || (Resto == 11)) Resto = 0;
+    if (Resto != parseInt(cpf.substring(9, 10))) return false;
 
-	Soma = 0;
-	for (i = 1; i <= 10; i++) Soma = Soma + parseInt(cpf.substring(i - 1, i)) * (12 - i);
-	Resto = (Soma * 10) % 11;
+    Soma = 0;
+    for (i = 1; i <= 10; i++) Soma = Soma + parseInt(cpf.substring(i - 1, i)) * (12 - i);
+    Resto = (Soma * 10) % 11;
 
-	if ((Resto == 10) || (Resto == 11)) Resto = 0;
-	if (Resto != parseInt(cpf.substring(10, 11))) return false;
-	return true;
+    if ((Resto == 10) || (Resto == 11)) Resto = 0;
+    if (Resto != parseInt(cpf.substring(10, 11))) return false;
+    return true;
 }
 
 function validarCNPJ(cnpj) {
- 
-    cnpj = cnpj.replace(/[^\d]+/g,'');
- 
-    if(cnpj == '') return false;
-     
+
+    cnpj = cnpj.replace(/[^\d]+/g, '');
+
+    if (cnpj == '') return false;
+
     if (cnpj.length != 14)
         return false;
- 
+
     // Elimina CNPJs invalidos conhecidos
-    if (cnpj == "00000000000000" || 
-        cnpj == "11111111111111" || 
-        cnpj == "22222222222222" || 
-        cnpj == "33333333333333" || 
-        cnpj == "44444444444444" || 
-        cnpj == "55555555555555" || 
-        cnpj == "66666666666666" || 
-        cnpj == "77777777777777" || 
-        cnpj == "88888888888888" || 
+    if (cnpj == "00000000000000" ||
+        cnpj == "11111111111111" ||
+        cnpj == "22222222222222" ||
+        cnpj == "33333333333333" ||
+        cnpj == "44444444444444" ||
+        cnpj == "55555555555555" ||
+        cnpj == "66666666666666" ||
+        cnpj == "77777777777777" ||
+        cnpj == "88888888888888" ||
         cnpj == "99999999999999")
         return false;
-         
+
     // Valida DVs
     tamanho = cnpj.length - 2
-    numeros = cnpj.substring(0,tamanho);
+    numeros = cnpj.substring(0, tamanho);
     digitos = cnpj.substring(tamanho);
     soma = 0;
     pos = tamanho - 7;
     for (i = tamanho; i >= 1; i--) {
-      soma += numeros.charAt(tamanho - i) * pos--;
-      if (pos < 2)
+        soma += numeros.charAt(tamanho - i) * pos--;
+        if (pos < 2)
             pos = 9;
     }
     resultado = soma % 11 < 2 ? 0 : 11 - soma % 11;
     if (resultado != digitos.charAt(0))
         return false;
-         
+
     tamanho = tamanho + 1;
-    numeros = cnpj.substring(0,tamanho);
+    numeros = cnpj.substring(0, tamanho);
     soma = 0;
     pos = tamanho - 7;
     for (i = tamanho; i >= 1; i--) {
-      soma += numeros.charAt(tamanho - i) * pos--;
-      if (pos < 2)
+        soma += numeros.charAt(tamanho - i) * pos--;
+        if (pos < 2)
             pos = 9;
     }
     resultado = soma % 11 < 2 ? 0 : 11 - soma % 11;
     if (resultado != digitos.charAt(1))
-          return false;
-           
+        return false;
+
     return true;
-    
+
 }
